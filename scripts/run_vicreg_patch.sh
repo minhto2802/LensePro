0cfa10f00aac6349ba0f7e61f162a07c56531fbd
tnnha <minhto2802@gmail.com>
2024-01-21 21:02:40 -0500

    Rename files

--- scripts/run_vicreg_patch.sh
+++ scripts/run_vicreg_patch.sh
@@ -8,13 +8,13 @@ epochs_1=200
 
 for sd in $3; do
   cfg=vicreg_patch.yml
-  cmd_tr=$(echo vicreg_rf_patches.py -cfg ${cfg} --exp-suffix _sd${sd}${exp_suffix} --random-crop --infomin \
+  cmd_tr=$(echo main_pretrain.py -cfg ${cfg} --exp-suffix _sd${sd}${exp_suffix} --random-crop --infomin \
   --epochs ${epochs_1} --seed ${sd}) #  --optimizer adamw --base-lr 0.0002)
   echo "${cmd_tr}"
   python ${cmd_tr}
 
 #  cfg=vicreg_patch.yml
-#  cmd_tr=$(echo vicreg_rf_patches.py -cfg ${cfg} --exp-suffix _sd${sd}${exp_suffix} --epochs ${epochs_2} --seed ${sd}) # --optimizer adamw --base-lr 0.0002)
+#  cmd_tr=$(echo main_pretrain.py -cfg ${cfg} --exp-suffix _sd${sd}${exp_suffix} --epochs ${epochs_2} --seed ${sd}) # --optimizer adamw --base-lr 0.0002)
 #  echo "${cmd_tr}"
 #  python ${cmd_tr}
 
